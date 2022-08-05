70005b61c26d8b69dad88e875ebfa7dac50725ad
RandhoB <randho.bagas12@gmail.com>
2022-08-05 21:16:39 +0700

    perubahan lanjutan

--- lib/app/modules/profile_page/views/contact_us_view.dart
+++ lib/app/modules/profile_page/views/contact_us_view.dart
@@ -9,162 +9,166 @@ class ContactUsView extends GetView {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 25, right: 25, left: 25, bottom: 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: Image.asset(
-                      "assets/icons/Back2.png",
-                      width: 30,
-                      height: 30,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 25, right: 25, left: 25, bottom: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Get.back();
+                      },
+                      child: Image.asset(
+                        "assets/icons/Back2.png",
+                        width: 30,
+                        height: 30,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    "Need help?",
-                    style: TextStyle(
-                      color: WTextTopUp,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    "24/7\nHelp Center",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Image.asset(
-                    "assets/images/man-chatting.png",
-                    height: MediaQuery.of(context).size.height * 0.23,
-                  )
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.58,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+                  ],
                 ),
-                color: Color(0xffFEDAC9),
               ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+              Center(
                 child: Column(
                   children: [
                     Text(
-                      "Tell us how we can help",
+                      "Need help?",
                       style: TextStyle(
-                        color: Color(0xff363030),
+                        color: WTextTopUp,
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     SizedBox(
-                      height: 16,
-                    ),
-                    Center(
-                      child: Text(
-                        "our crew of superheroes standing by\nfor service and help",
-                        textAlign: TextAlign.center,
-                        style:
-                            TextStyle(fontSize: 15, color: Color(0xff655E5E)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    itemCS(
-                      nama: "Customer Service 1",
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    itemCS(
-                      nama: "Customer Service 2",
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    itemCS(
-                      nama: "Customer Service 3",
-                    ),
-                    SizedBox(
-                      height: 53,
-                    ),
-                    Text(
-                      "Mail & Information",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff645959),
-                      ),
-                    ),
-                    SizedBox(
                       height: 5,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          "assets/images/mail.png",
-                          width: 23,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "admin@boxkoo.co.id",
-                          style: TextStyle(
-                            color: Color(0xff645959),
-                          ),
-                        )
-                      ],
+                    Text(
+                      "24/7\nHelp Center",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          "assets/images/website.png",
-                          width: 23,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "boxkoo.co.id",
-                          style: TextStyle(
-                            color: Color(0xff645959),
-                          ),
-                        )
-                      ],
+                    SizedBox(
+                      height: 10,
                     ),
+                    Image.asset(
+                      "assets/images/man-chatting.png",
+                      height: MediaQuery.of(context).size.height * 0.23,
+                    )
                   ],
                 ),
               ),
-            )
-          ],
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.58,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  color: Color(0xffFEDAC9),
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 40),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Tell us how we can help",
+                          style: TextStyle(
+                            color: Color(0xff363030),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Center(
+                          child: Text(
+                            "our crew of superheroes standing by\nfor service and help",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 15, color: Color(0xff655E5E)),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        itemCS(
+                          nama: "Customer Service 1",
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        itemCS(
+                          nama: "Customer Service 2",
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        itemCS(
+                          nama: "Customer Service 3",
+                        ),
+                        SizedBox(
+                          height: 53,
+                        ),
+                        Text(
+                          "Mail & Information",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff645959),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "assets/images/mail.png",
+                              width: 23,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "admin@boxkoo.co.id",
+                              style: TextStyle(
+                                color: Color(0xff645959),
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "assets/images/website.png",
+                              width: 23,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "boxkoo.co.id",
+                              style: TextStyle(
+                                color: Color(0xff645959),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
